e2f6b83c1218fea5c7452971d908f8c5ee6b91a0
stefan <cecavac98@gmail.com>
2025-02-02 16:07:22 +0100

    Added refresh token endpoint

--- server/src/controllers/AuthController.ts
+++ server/src/controllers/AuthController.ts
@@ -1,5 +1,8 @@
 import { NextFunction, Request, Response } from "express";
 import { loginUserService, registerUserService } from "../services/AuthService";
+import jwt from "jsonwebtoken";
+import { generateAccessToken } from "../util/GenerateAccessToken";
+import { CustomError } from "../middlewares/ErrorHandler";
 
 export async function registerUser(req: Request, res: Response, next: NextFunction) {
   const { email, password } = req.body;
@@ -7,7 +10,7 @@ export async function registerUser(req: Request, res: Response, next: NextFuncti
   try {
     const { accessToken, refreshToken } = await registerUserService({ email, password });
 
-    res.cookie("refresh_token", refreshToken, {
+    res.cookie("refreshToken", refreshToken, {
       httpOnly: true,
       secure: true,
       maxAge: 60000,
@@ -25,11 +28,29 @@ export async function loginUser(req: Request, res: Response, next: NextFunction)
   try {
     const { accessToken, refreshToken } = await loginUserService({ email, password });
 
-    res.cookie("refresh_token", refreshToken, {
+    res.cookie("refreshToken", refreshToken, {
       httpOnly: true,
       secure: true,
       maxAge: 60000,
     });
+
+    res.status(201).json({ accessToken });
+  } catch (error) {
+    next(error);
+  }
+}
+
+export async function refreshToken(req: Request, res: Response, next: NextFunction) {
+  const refreshToken = req.cookies.refreshToken;
+
+  try {
+    if (!refreshToken) throw new CustomError("No token proivded", 401);
+    const decodedToken = jwt.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET as string);
+
+    console.log("decoded token", decodedToken);
+
+    if (!decodedToken) throw new CustomError("Token expired or not valid", 403);
+    const accessToken = generateAccessToken((decodedToken as any).id);
 
     res.status(201).json({ accessToken });
   } catch (error) {
--- server/src/routes/AuthRoute.ts
+++ server/src/routes/AuthRoute.ts
@@ -1,9 +1,10 @@
 import express from "express";
-import { loginUser, registerUser } from "../controllers/AuthController";
+import { loginUser, refreshToken, registerUser } from "../controllers/AuthController";
 
 const router = express.Router();
 
 router.post("/register", registerUser);
 router.post("/login", loginUser);
+router.post("/refresh-token", refreshToken);
 
 export default router;
