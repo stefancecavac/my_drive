dacb496201ebac483b84d8542e1ceb031fdcf1c3
stefan <cecavac98@gmail.com>
2025-02-02 16:57:41 +0100

    init client

---
+++ client/src/App.tsx
@@ -0,0 +1,15 @@
+function App() {
+  return (
+    <div className="flex flex-1 h-screen w-screen bg-base-300 ">
+      <button className="btn btn-primary   "> hello</button>
+      <button className="btn btn-secondary   "> hello</button>
+      <button className="btn btn-accent   "> hello</button>
+      <p className="text-neutral">
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Impedit dicta tempore adipisci inventore accusamus ipsum voluptas voluptatibus. Vero
+        dolores in rerum fugit omnis aliquid nemo quis, nobis id architecto earum.
+      </p>
+    </div>
+  );
+}
+
+export default App;
